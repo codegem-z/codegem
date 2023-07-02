4fce46998c1cc18bbb86526d34111d54315a3ae9
ben <x_z_gfree@163.com>
2023-07-02 14:21:56 +0800

    chore: 0.1.1-beta.1

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,5 @@
 import { Option } from './type.js';
-export { default as Generator } from './generator';
+export { default as Generator } from './generator.js';
 
 export function defineConfig(config: Option) {
   return config;
